f5cbc0c9cc6b1a893249d0961306ed20f0f5dd45
taiyou <hiromacha1116@icloud.com>
2023-11-13 23:07:29 +0900

    画面出力OK

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,55 @@
 import './App.css';
 import { Sandpack } from '@codesandbox/sandpack-react';
+// import { nightOwl } from "@codesandbox/sandpack-themes";
 import P5Wrapper from './View';
 
 const files = {
-  '/App.js': `
-    import { useState } from 'react'
-
-    function App() {
-      const [count, setCount] = useState(0)
+  '/View.tsx': `
+    import React, { useEffect, useRef } from 'react';
+    import p5 from 'p5';
     
+    const P5Wrapper = () => {
+      const sketchRef = useRef<HTMLDivElement | null>(null);
+    
+      const Sketch = (p: p5) => {
+        p.setup = () => {
+          p.createCanvas(400, 400);
+        };
+    
+        p.draw = () => {
+          p.background(220);
+          p.ellipse(p.width / 2, p.height / 2, 50, 50);
+        };
+      };
+    
+      useEffect(() => {
+        // sketchRef.currentがnullでないことを確認
+        if (sketchRef.current) {
+          const canvas = new p5(Sketch, sketchRef.current);
+    
+          return () => {
+            canvas.remove();
+          };
+        }
+      }, []);
+    
+      return <div ref={sketchRef}></div>;
+    };
+    
+    export default P5Wrapper;
+  `,
+  '/App.js': `
+    import React from 'react';
+    import P5Wrapper from './View';
+
+    const App = () => {
       return (
         <div>
-          <h1>Vite + React</h1>
-          <div>
-            <button onClick={() => setCount((count) => count + 1)}>
-              count is {count}
-            </button>
-          </div>
+          <P5Wrapper />
         </div>
-      )
-    }
+      );
+    };
+
     export default App;
   `,
 };
@@ -28,7 +58,15 @@ function App() {
   return (
     <div className='App'>
       {/* <P5Wrapper /> */}
-      <Sandpack template='react' files={files} />
+      <Sandpack 
+        template='react' 
+        files={files}
+        customSetup={{
+          dependencies: {
+            "p5": "^1.8.0",
+          }
+        }}
+      />
       <p>自由にコードを変更してください。</p>
     </div>
   );
