10ad45123a14667090e9309169f461b3d948d537
taiyou <hiromacha1116@icloud.com>
2023-11-14 22:18:38 +0900

    ファイルインストール

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,10 @@
 import './App.css';
 import { Sandpack } from '@codesandbox/sandpack-react';
-// import { nightOwl } from "@codesandbox/sandpack-themes";
-import P5Wrapper from './View';
+import { nightOwl } from "@codesandbox/sandpack-themes";
+import { P5WrapperSource } from './View';
 
 const files = {
-  '/View.tsx': `
-    import React, { useEffect, useRef } from 'react';
-    import p5 from 'p5';
-    
-    const P5Wrapper = () => {
-      const sketchRef = useRef<HTMLDivElement | null>(null);
-    
-      const Sketch = (p: p5) => {
-        p.setup = () => {
-          p.createCanvas(400, 400);
-        };
-    
-        p.draw = () => {
-          p.background(220);
-          p.ellipse(p.width / 2, p.height / 2, 50, 50);
-        };
-      };
-    
-      useEffect(() => {
-        // sketchRef.currentがnullでないことを確認
-        if (sketchRef.current) {
-          const canvas = new p5(Sketch, sketchRef.current);
-    
-          return () => {
-            canvas.remove();
-          };
-        }
-      }, []);
-    
-      return <div ref={sketchRef}></div>;
-    };
-    
-    export default P5Wrapper;
-  `,
+  '/View.tsx': P5WrapperSource,
   '/App.js': `
     import React from 'react';
     import P5Wrapper from './View';
@@ -57,7 +24,6 @@ const files = {
 function App() {
   return (
     <div className='App'>
-      {/* <P5Wrapper /> */}
       <Sandpack 
         template='react' 
         files={files}
@@ -66,6 +32,7 @@ function App() {
             "p5": "^1.8.0",
           }
         }}
+        theme={nightOwl}
       />
       <p>自由にコードを変更してください。</p>
     </div>
