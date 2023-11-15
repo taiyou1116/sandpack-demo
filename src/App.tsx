7d9f0f838903ccdd4c4c54d6cbd068de3cae885e
taiyou <hiromacha1116@icloud.com>
2023-11-15 17:45:07 +0900

    sandpack拡張

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import './App.css';
-import { Sandpack, SandpackLayout } from '@codesandbox/sandpack-react';
-import { nightOwl } from "@codesandbox/sandpack-themes";
+import { SandpackLayout } from '@codesandbox/sandpack-react';
+import { nightOwl, aquaBlue } from "@codesandbox/sandpack-themes";
 import { P5WrapperSource } from './View';
 import { 
   SandpackProvider, 
   SandpackCodeEditor,
-  SandpackPreview,
-  useSandpackNavigation, 
+  SandpackPreview, 
   } from '@codesandbox/sandpack-react';
 
 const files = {
-  '/View.tsx': P5WrapperSource,
+  '/View.tsx': {
+    code: P5WrapperSource,
+    readOnly: true,
+  },
   '/App.js': `
     import React from 'react';
     import P5Wrapper from './View';
@@ -38,12 +40,21 @@ function App() {
             "p5": "^1.8.0",
           }
         }}
-        theme={nightOwl}
-        className=''
+        theme={aquaBlue}
+        options={{
+          visibleFiles: ["/App.js", "/View.tsx"],
+          activeFile: "/App.js",
+        }}
       >
         <SandpackLayout className=' flex flex-col w-2/3 h-[calc(100vh-4rem)]' >
           <SandpackPreview className=' h-1/2' />
-          <SandpackCodeEditor className=' h-1/2' />
+          <SandpackCodeEditor
+          showTabs
+          showLineNumbers={true}
+          showInlineErrors
+          wrapContent
+          closableTabs
+          className=' h-1/2' />
         </SandpackLayout>
       </SandpackProvider>
     </div>
