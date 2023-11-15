56d8d9f074367b54e31e692ab5198c7c1b3c164e
taiyou <hiromacha1116@icloud.com>
2023-11-15 23:34:27 +0900

    カスタム

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import fs from 'fs';
-import path from 'path';
 
-const customPlugin = {
-  name: 'custom-load-raw-files',
-  // この関数は、モジュールが読み込まれる際に呼ばれる
+const rawFilePlugin = {
+  name: 'raw-file-loader',
+  enforce: 'pre' as const,
   load(id) {
-    // 指定された拡張子のファイルの場合に処理
     if (id.endsWith('.view.tsx')) {
-      // ファイルの内容を文字列として読み込む
       const contents = fs.readFileSync(id, 'utf-8');
-      // JavaScriptの文字列としてエクスポート
       return `export default ${JSON.stringify(contents)}`;
     }
   }
@@ -20,8 +16,6 @@ const customPlugin = {
 export default defineConfig({
   plugins: [
     react(),
-    customPlugin
-    // 他のプラグイン
-  ],
-  // その他の設定
+    // rawFilePlugin
+  ]
 });
